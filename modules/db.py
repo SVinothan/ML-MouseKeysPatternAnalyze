276b558fb101c7305239c5858d4e779325dd0223
SVinothan <svinothan09@gmail.com>
2025-08-06 17:25:49 +0530

    environment changes

--- modules/db.py
+++ modules/db.py
@@ -4,8 +4,11 @@ from pymongo import MongoClient
 from datetime import datetime
 
 # load_dotenv()
-client = MongoClient('mongodb://localhost:27017')
-db = client['laravel_app']
+# client = MongoClient(os.getenv("MONGO_URI"))
+# db = client[os.getenv("MONGO_DB")]
+
+client = MongoClient('mongodb+srv://svinothand:11Xowxj5mzHFHWNs@clusterml.aujznm6.mongodb.net?ssl=true&authSource=admin')
+db = client['sample_mflix']
 
 def get_mouse_data(user_id, today):
     all_data = list(db.mouse_positions.find({"user_id": user_id}))
